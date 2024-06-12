ecc5372b0914a7a9fd5d33003b94071f0de5c326
Davi Morais <davifarias2433@gmail.com>
2024-06-12 16:48:57 -0300

    Estilizando e adicionando estado para CaixaPesquisa

--- src/components/CaixaPesquisa/index.tsx
+++ src/components/CaixaPesquisa/index.tsx
@@ -1,10 +1,37 @@
-import lupa from '../../assets/Vectorlupa.svg'
+import { useState } from 'react';
+import lupa from '../../assets/Vectorlupa.svg';
+import styles from "./CaixaPesquisa.module.css";
 
 export default function CaixaPesquisa() {
-    return (
-      <div>
-        <img src={lupa} alt="buscar" />
-        <input type="text" placeholder='Pesquisar Eventos'/>
-      </div>
-    );
+
+  const [focus, setFocus] = useState(false);
+  const [text, setText] = useState("");
+
+  function inputFocus() {
+    setFocus(true)
+  }
+
+  function inputBlur() {
+    setFocus(false)
+  }
+
+  function handleText(text:string) {
+    setText(text)
+  }
+
+
+  return (
+    <div className={focus ? styles.containerOnFocus : styles.container}>
+      <img src={lupa} alt="buscar" className='lupaImage' />
+      <input
+        onFocus={inputFocus}
+        onBlur={inputBlur}
+        type="text" 
+        value={text}
+        onChange={(e) => handleText(e.target.value)}
+        placeholder='Pesquisar Eventos' 
+        className={styles.searchInput}
+      />
+    </div>
+  );
   }
