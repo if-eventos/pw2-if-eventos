22d2ab909b0e5f325c2c90aa02a0f162672de455
Davi Morais <davifarias2433@gmail.com>
2024-06-12 19:48:28 -0300

    Adicionando estilo ao Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,16 +1,22 @@
 import CaixaPesquisa from '../CaixaPesquisa';
 import logo from '../../assets/IF_logo_BLUElogo-if-eventos.svg';
+import styles from './Menu.module.css';
 
 
 export default function Menu() {
     return (
-      <div>
-        <img src={logo} alt="Home" />
+      <div className={styles.container}>
+        <div className={styles.leftMenu}>
+          <img src={logo} alt="Home" />
 
-        <CaixaPesquisa />
+          <CaixaPesquisa />
+        </div>
 
-        <button>Login</button>
-        <button>Cadastre-se</button>
+
+        <div className={styles.rightMenu}>
+          <button>Login</button>
+          <button>Cadastre-se</button>
+        </div>
       </div>
     );
   }
