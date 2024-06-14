978806c1d0f2cc62d7d8b7029e1af24bacc990fb
Davi Morais <davifarias2433@gmail.com>
2024-06-14 14:56:35 -0300

    Criando arquivo da pagina Cadastro

---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Cadastro() {
+    return (
+        <main>
+            {/* Form Cadastro Aqui */}
+            <div>
+                <p>você já possui uma conta?</p>
+                <p>Acesse ela por <a href="#">aqui</a></p>
+            </div>
+        </main>
+    )
+}
