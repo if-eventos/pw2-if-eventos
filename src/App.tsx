b7847eeaf1ea9160fdb828342697288bed0f68a1
Davi Morais <davifarias2433@gmail.com>
2024-08-23 21:21:51 -0300

    Add Auth Context
    Agora os componentes da aplicacao poderao acessar os dados do
    usuario se ele estiver logado. Ademais, foram adicionados metodos
    para o usuario logar e deslogar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Menu from "./components/Menu"
+import { AuthProvider } from "./context/AuthContext"
 import './App.module.css'
 
 function App() {
 
   return (
-    <>
+    <AuthProvider>
       <Menu />
-    </>
+    </AuthProvider>
   )
 }
 
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,109 @@
+import { createContext, useEffect, useState } from "react"
+import { api } from "../api/axios"
+
+interface User {
+    id: number
+    name: string,
+    email: string,
+    telefone: string,
+    ehPalestrante: number,
+    minicurriculo: string,
+    urlsite: string,
+    curriculo_redesocial: string,
+    image: string
+}
+interface getUser {
+    name: string,
+    email: string,
+    telefone: string,
+    ehPalestrante: number,
+    minicurriculo: string,
+    urlsite: string,
+    curriculo_redesocial: string,
+    image: string
+}
+
+
+
+interface signinResponse {
+    token: string,
+    idUser: string
+}
+
+interface IAuthContext {
+    user: User | null
+    logar: (email:string, password:string) => Promise<void>
+    deslogar: () => Promise<void>
+}
+
+export const AuthContext = createContext({} as IAuthContext)
+
+
+type Props = {
+    children: React.ReactNode
+}
+
+export function AuthProvider({children}:Props) {
+
+    const [user, setUser] = useState<User | null>(null)
+
+
+    async function logar(email:string, password:string) {
+        try {
+            const data = {
+                email: email,
+                password: password
+            }
+            const response =  await api.post('/api/v1/user/signin', data)
+
+            if (response.status === 200) {
+                const {token, idUser} = response.data as signinResponse
+                api.defaults.headers.common.Authorization = `Bearer ${token}`
+
+                const getUserResponse = await api.get("/api/v1/user/", {
+                    params: {
+                        id: `${idUser}`
+                    }
+                })
+
+                const userData = getUserResponse.data as getUser
+                const id = Number(idUser)
+                const user = {...userData, id:id}
+                setUser(user)
+
+                localStorage.setItem('auth.token', token)
+                localStorage.setItem('auth.user', JSON.stringify(user))
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+    async function deslogar() {
+        setUser(null)
+        localStorage.removeItem('auth.token')
+        localStorage.removeItem('auth.user')
+        api.defaults.headers.common.Authorization = undefined //maybe it's wrong...
+    }
+
+
+    useEffect(() => {
+        const token = localStorage.getItem('auth.token')
+        console.log(token)
+        const user = localStorage.getItem('auth.user')
+
+        if (token && user) {
+            api.defaults.headers.common.Authorization = token
+            setUser(JSON.parse(user))
+        }
+    }, [])
+
+
+
+    return (
+        <AuthContext.Provider value={{user, logar, deslogar}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
