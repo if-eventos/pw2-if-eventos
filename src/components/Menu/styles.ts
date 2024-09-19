3b93ea7bcaa8d1c11a0a7a5e6625c43a924b68f8
Davi Morais <davifarias2433@gmail.com>
2024-09-19 16:30:45 -0300

    Add responsividade inicial do Menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -7,18 +7,91 @@ export const Container = styled.div`
     align-items: center;
     justify-content: space-between;
     box-shadow: 0px 0px 10px black;
+
+
+    @media (max-width: 810px) {
+      flex-flow: row wrap;
+      height: max-content;
+    }
 `
 
-export const LeftMenu = styled.div`
+export const LeftContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
   padding-left: 89px;
+
+  @media (max-width: 810px) {
+    padding-left: 0px;
+  }
 `;
 
-export const AuthMenu = styled.div`
+export const AuthContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
   padding-right: 34px;
+
+  @media (max-width: 810px) {
+    width: 100%;
+    padding-right: 0px;
+
+
+  }
+`;
+
+
+export const OpenMenu = styled.div`
+
+  display: none;
+
+  @media (max-width: 810px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 64px;
+    height: 64px;
+
+    cursor: pointer;
+  }
+`;
+
+
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: max-content;
+  height: max-content;
+
+  /* cursor: pointer; */
+
+
+  @media (max-width: 810px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`;
+
+
+export const PagesContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: max-content;
+  height: max-content;
+
+  cursor: pointer;
+
+
+  @media (max-width: 810px) {
+    width: 100%;
+    background-color: bisque;
+    flex-direction: column;
+    align-items: start;
+  }
 `;
