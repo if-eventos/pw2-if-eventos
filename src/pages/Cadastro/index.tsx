66e7c837dd8ba05f2317f0db97f28dcef21599a2
Davi Morais <davifarias2433@gmail.com>
2024-09-24 11:42:54 -0300

    Estilizando inputs do Cadastro
    e centralizando elementos da pagina

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -6,6 +6,17 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { ImageUploader } from "../../components/ImageUploader"
 import { SubmitButton } from "../../components/Buttons"
+import { InputStyled } from "../../components/Input"
+import { Container, 
+        FormStyled } from "./styles"
+
+import icon_email from "../../assets/icon_email.png"
+import icon_password from "../../assets/icon_password.png"
+import icon_phone from "../../assets/icon_phone.png"
+import icon_profile from "../../assets/icon_profile.png"
+import icon_redesocial from "../../assets/icon_redesocial.png"
+import icon_site from "../../assets/icon_site.png"
+
 
 
 export default function Cadastro() {
@@ -67,28 +78,27 @@ export default function Cadastro() {
 
 
     return (
-        <div >
-            <form style={{display: 'flex', flexDirection: 'column'}}
-             onSubmit={handleSubmit((data) => handleSignup(data))}>
+        <Container >
+            <FormStyled onSubmit={handleSubmit((data) => handleSignup(data))}>
 
                 <ImageUploader image={image} setImage={setImage} />
 
-                <input {...register("name")} placeholder="nome"  />
+                <InputStyled $iconPath={icon_profile} {...register("name")} placeholder="nome"  />
                 {errors.name?.message && <div>{errors.name.message}</div>}
 
-                <input {...register("email")} placeholder="email" />
+                <InputStyled $iconPath={icon_email} {...register("email")} placeholder="email" />
                 {errors.email?.message && <div>{errors.email.message}</div>}
                 
-                <input {...register("telefone")} placeholder="telefone" />
+                <InputStyled $iconPath={icon_phone} {...register("telefone")} placeholder="telefone" />
                 {errors.telefone?.message && <div>{errors.telefone.message}</div>}
                 
-                <input {...register("urlsite")} placeholder="site pessoal:" />
+                <InputStyled $iconPath={icon_site} {...register("urlsite")} placeholder="site pessoal:" />
                 {errors.urlsite?.message && <div>{errors.urlsite.message}</div>}
 
-                <input {...register("curriculo_redesocial")} placeholder="rede social" />
+                <InputStyled $iconPath={icon_redesocial} {...register("curriculo_redesocial")} placeholder="rede social" />
                 {errors.curriculo_redesocial?.message && <div>{errors.curriculo_redesocial.message}</div>}
                 
-                <input {...register("password")} placeholder="senha" type="password"/>
+                <InputStyled $iconPath={icon_password} {...register("password")} placeholder="senha" type="password"/>
                 {errors.password?.message && <div>{errors.password.message}</div>}
 
                 <input {...register("minicurriculo")} placeholder="Escreva um pouco sobre você:" />
@@ -101,12 +111,12 @@ export default function Cadastro() {
                 <SubmitButton>
                     Criar Conta!
                 </SubmitButton>
-            </form>
+            </FormStyled>
 
             <div>
                 <p>você já possui uma conta?</p>
                 <p>Acesse ela por <a href="#">aqui</a></p>
             </div>
-        </div>
+        </Container>
     )
 }
