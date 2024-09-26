ed46ad958211b3b0e093b52a31455d846020d8e7
Davi Morais <davifarias2433@gmail.com>
2024-09-25 21:53:52 -0300

    Consertando responsividade

--- src/pages/Cadastro/styles.ts
+++ src/pages/Cadastro/styles.ts
@@ -4,13 +4,15 @@ import styled from "styled-components"
 
 export const Container = styled.div`
     width: 100%;
-    height: 80vh;
+    height: fit-content;
 
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    /* justify-content: center; */
 
+    padding-top: 20px;
+    padding-bottom: 100px;
 `
 
 
