6dd4627c7c5872135b9db4483b8d82dd4a586cf1
Davi Morais <davifarias2433@gmail.com>
2024-09-27 11:01:23 -0300

    update

--- src/components/Titulo/index.ts
+++ src/components/Titulo/index.ts
@@ -4,4 +4,6 @@ export const Titulo = styled.h3`
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-weight: normal;
     color: darkgray;
+
+    text-align: center;
 `
