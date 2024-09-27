f5ef89621482d0740b327f826f5d893e53386316
Davi Morais <davifarias2433@gmail.com>
2024-09-27 10:56:24 -0300

    Estilizando Titulo

--- src/components/Titulo/index.ts
+++ src/components/Titulo/index.ts
@@ -1,8 +1,7 @@
 import styled from "styled-components"
 
-export const Titulo = styled.h1`
+export const Titulo = styled.h3`
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-weight: normal;
-
-    margin-top: 30px;
+    color: darkgray;
 `
